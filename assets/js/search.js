18990cf56c24825136af284c34f6cbb1de358fc6
bo-lu <bo-lu@users.noreply.github.com>
2021-01-29 02:00:52 +0000

    deploy: 752d0352c51de4b4fc4a34ee2ebe4210201aa68b

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/geoca_pycsw_howto/assets/js/search-data.json', true);
+    request.open('GET', '/geoca_developers_page/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
